9ea7ddb9e869e9708f1241a05f719b335ac97b3c
Angelo Lamacchia <angelo.lamacchia@live.it>
2021-02-27 13:34:54 +0100

    aggiunto controllo num 1a5

--- Dispari/js/script.js
+++ Dispari/js/script.js
@@ -7,39 +7,46 @@ Dichiariamo se l'utente ha vinto.*/
 //prompt di scelta tra pari o dispari
 var sceltaPariDispari = prompt("inserire pari o dispari");
 
-//primpt di scelta da 1 a 5
-var scelta1a5 = parseInt( prompt("Inserire un numero da 1 a 5") );
-
-//genero un numero random da 1 a 5
-var numeroRandom5 = numeroRandom(5);
-
-// console.log(numeroRandom5);
-
-//sommo il numero utente con quello random
-var somma = scelta1a5 + numeroRandom5;
-
+//controllo che la parola sia pari o dispari
 if (sceltaPariDispari != "pari" && sceltaPariDispari != "dispari") {
     alert("errore inserire la parola pari o dispari");
     alert("Ritenti?");
     location.reload();
 } else {
-    //definisco una variabile se è pari o dispari
-    var pariDispari;
-    console.log(somma);
-    if (numeroDispari(somma) == true) {
-        pariDispari = "dispari";
-        alert("La somma dei numeri è: " + somma + " che è dispari");
-    } else {
-        pariDispari = "pari";
-        alert("La somma dei numeri è: " + somma + " che è pari");
-    }
+    //creo la var prompt di scelta da 1 a 5
+    var scelta1a5 = parseInt( prompt("Inserire un numero da 1 a 5") );
 
-    //confronto il risultato con quello dell'utente
-    if (sceltaPariDispari == pariDispari) {
-        alert("Hai vinto");
+    //genero un numero random da 1 a 5
+    var numeroRandom5 = numeroRandom(5);
+    alert("Il mio numero è: " + numeroRandom5);
+
+    //sommo il numero utente con quello random
+    var somma = scelta1a5 + numeroRandom5;
+
+    //controllo che il numero sia tra 1 e 5
+    if (scelta1a5==0 || scelta1a5>5) {
+        alert("errore inserire un numero da 1 a 5");
+        alert("Ritenti?");
+        location.reload();
     } else {
-        alert("Hai perso");
-    }
+        //definisco una variabile se è pari o dispari
+        var pariDispari;
+        console.log(somma);
+        if (numeroDispari(somma) == true) {
+            pariDispari = "dispari";
+            alert("La somma dei numeri è: " + somma + " che è dispari");
+        } else {
+            pariDispari = "pari";
+            alert("La somma dei numeri è: " + somma + " che è pari");
+        }
+
+        //confronto il risultato con quello dell'utente
+        if (sceltaPariDispari == pariDispari) {
+            alert("Hai vinto");
+        } else {
+            alert("Hai perso");
+        }
+    }   
 }
 
 
